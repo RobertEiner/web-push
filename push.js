2288ead75397da7b6539b486916a881468ba7507
Robert <guseinero@student.gu.se>
2023-07-27 15:33:06 +0200

    a

--- push.js
+++ push.js
@@ -14,12 +14,13 @@ push.setVapidDetails(
 
 let sub = {
   endpoint:
-    "https://wns2-db5p.notify.windows.com/w/?token=BQYAAABQ0Qc%2f22bsD%2f0BE0TozDH6x6GudOtH2mgrDs%2f3BnQkEtk%2fpW6MQhKSwXRCy9vHu4DLYs6G4r6eB9FXbnrCi4FQOtt6dv78gaQyK5cF5OE2RJJfJWfbFuAkCOb2%2bLlaXPHVUpis3Ly0vaxbsoZLTq64JHBzIcmWDQPuZoDp6lk%2fccloAwNRdw84uAxoVH8l24pCVw%2bYKW46S1WqRk0Q8MqSS6%2faWilFsG7kwUt79ys%2fTAR%2b1BI%2bmKE9lnpXRms7Z94nwoQ4XLe0a6xcWi0kunYJuI2N%2bmova%2f2cHqZFsb5kM6HkQEGNDcBF%2fiZvz%2fgoVIZFK4C34k6RgxSVitPyUtUu",
+    "https://fcm.googleapis.com/fcm/send/ft2ZZyZDkvg:APA91bF7f_7hZqMMKaN5sv_2f675vrbbLN_DSxEQtLtCcr-esL_QVjYKMq4-flnxKa2n3HTIqAwI1ONCIF5Afphg2XESg0Bvy11J-HaweVOtn45iRF4DgxPklRXj5HAqvF4bWhj0XobQ",
   expirationTime: null,
   keys: {
     p256dh:
-      "BBV_Gl2bRIR2e_q1NlWeRegNmGH_6c9050Im2tjtHzL6Vep1ObP_SDm4t_32MJHkGQSeg2fgZGL35Tah2ymLgMI",
-    auth: "7HPOcI-KiZWAmKVr5E3i-w",
+      "BM8qva-_5XSNF8fFxJX5IAN0X7YOwyVJ4lE-AwcnJhcAP5gCFYnm0hPotf3EtWI50nXa2XOAsAWzeav0Q5RTppY",
+    auth: "Hj2Kk7K6E6MFpgjBzOuF7Q",
   },
 };
+
 push.sendNotification(sub, "test message");
