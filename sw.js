965c6b74ab485d53c42ccf01ce026a6754d6b2e9
Robert <guseinero@student.gu.se>
2023-07-27 15:02:44 +0200

    h

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-// self.addEventListener('push', () => {
-//     self.registration.showNotification('Test message', {
-//         body: 'hey there'
-//     });
-// });
+self.addEventListener('push', () => {
+    self.registration.showNotification('Test message', options);
+});
 
-self.addEventListener('push', function(event) {
-    event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {
-      body: 'Push Notification Subscription Management'
-    }));
-  });
+// self.addEventListener('push', function(event) {
+//     event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {
+//       body: 'Push Notification Subscription Management'
+//     }));
+//   });
